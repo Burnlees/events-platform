1d136e91bb2d97b0ea4921a0b649d44e641b263b
burnlees <josh.burnlees@outlook.com>
2025-01-06 13:11:48 +0000

    feat: creates basic nav component
    created basic placeholder nav component
    
    also introduced css themeing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,23 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+const TopNav = () => {
+  return (
+    <nav className="bg-primary flex w-full items-center justify-between px-4 py-2">
+      <div className="font-bold text-white">NextUp</div>
+    </nav>
+  );
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body>
+        <TopNav />
+        {children}
+      </body>
     </html>
   );
 }

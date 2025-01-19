b12ba8e390f3c7d0c8cafafd943d0500c0ed4b8e
burnlees <josh.burnlees@outlook.com>
2025-01-19 13:40:07 +0000

    perf: removes unused import

--- src/app/_components/EventsList.tsx
+++ src/app/_components/EventsList.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import EventCard from "./EventCard";
-import { currentUser } from "@clerk/nextjs/server";
 
 type EventsListProps = {
   getEvents: () => Promise<EventDetails[] | undefined>;
@@ -9,7 +8,7 @@ type EventsListProps = {
 const EventsList = async ({ getEvents }: EventsListProps) => {
   const events = await getEvents();
 
-  if (!events) return null;
+  if (!events) return <div>No events found.</div>;
 
   return (
     <div className="grid gap-4 xl:grid-cols-4">
